36b1e820ce8172704217ff4241702d527832496d
Benjamin Jeanjean <bjeanjean@thegobeliners.com>
2016-08-29 20:40:10 +0200

    Add travis files and test coverage

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Apsylone\ImgixBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('apsylone_imgix');
+        $rootNode
+            ->children()
+            ->scalarNode('default_source')
+            ->defaultValue('default')
+            ->end()
+            ->append($this->getSourcesNode())
+            ->end()
+        ;
+        return $treeBuilder;
+    }
+    private function getSourcesNode()
+    {
+        $treeBuilder = new TreeBuilder();
+        $node = $treeBuilder->root('sources');
+        $node
+            ->requiresAtLeastOneElement()
+            ->useAttributeAsKey('name')
+            ->prototype('array')
+            ->children()
+            ->arrayNode('domains')
+            ->isRequired()
+            ->requiresAtLeastOneElement()
+            ->prototype('scalar')
+            ->end()
+            ->end()
+            ->scalarNode('sign_key')
+            ->defaultValue('')
+            ->end()
+            ->scalarNode('shard_strategy')
+            ->defaultValue('crc')
+            ->validate()
+            ->ifNotInArray(array('crc', 'cycle'))
+            ->thenInvalid('Invalid shard strategy "%s"')
+            ->end()
+            ->end()
+            ->end()
+            ->end()
+        ;
+        return $node;
+    }
+}
